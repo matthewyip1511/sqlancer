106e33af4a5cba7d41a17119c3a1e808f65b851c
TheodoreKooo <theodorekoo@gmail.com>
2025-03-31 18:13:42 +0800

    Remove commented out code.

--- src/sqlancer/presto/PrestoErrors.java
+++ src/sqlancer/presto/PrestoErrors.java
@@ -111,20 +111,6 @@ public final class PrestoErrors {
 
     private static List<String> getFunctionErrors() {
         ArrayList<String> errors = SQLErrors.getFunctionErrorsHelper();
-        /*
-         * errors.add("SUBSTRING cannot handle negative lengths"); errors.add("is undefined outside [-1,1]"); // ACOS
-         * etc errors.add("invalid type specifier"); // PRINTF errors.add("argument index out of range"); // PRINTF
-         * errors.add("invalid format string"); // PRINTF errors.add("number is too big"); // PRINTF
-         * errors.add("Like pattern must not end with escape character!"); // LIKE
-         * errors.add("Could not choose a best candidate function for the function call \"date_part"); // date_part
-         * errors.add("extract specifier"); // date_part errors.add("not recognized"); // date_part
-         * errors.add("not supported"); // date_part errors.add("Failed to cast"); errors.add("Conversion Error");
-         * errors.add("Could not cast value"); errors.add("Insufficient padding in RPAD"); // RPAD
-         * errors.add("Could not choose a best candidate function for the function call"); // monthname
-         * errors.add("expected a numeric precision field"); // ROUND
-         * errors.add("with non-constant precision is not supported"); // ROUND
-         *
-         */
 
         errors.add("Unexpected parameters");
         errors.add("not registered");
