88c33168affe55935572055199d7e93c4cf15511
TheodoreKooo <theodorekoo@gmail.com>
2025-04-01 00:27:48 +0800

    Abstract out common code from generate() methods.

--- src/sqlancer/postgres/gen/PostgresIndexGenerator.java
+++ src/sqlancer/postgres/gen/PostgresIndexGenerator.java
@@ -5,13 +5,11 @@ import java.util.stream.Collectors;
 
 import sqlancer.Randomly;
 import sqlancer.SQLIndexGenerator;
-import sqlancer.common.DBMSCommon;
 import sqlancer.common.query.ExpectedErrors;
 import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.postgres.PostgresGlobalState;
 import sqlancer.postgres.PostgresSchema.PostgresColumn;
 import sqlancer.postgres.PostgresSchema.PostgresDataType;
-import sqlancer.postgres.PostgresSchema.PostgresIndex;
 import sqlancer.postgres.PostgresSchema.PostgresTable;
 import sqlancer.postgres.PostgresVisitor;
 import sqlancer.postgres.ast.PostgresExpression;
@@ -24,11 +22,7 @@ public final class PostgresIndexGenerator extends SQLIndexGenerator {
     public static SQLQueryAdapter generate(PostgresGlobalState globalState) {
         ExpectedErrors errors = SQLIndexGenerator.generateErrors();
         StringBuilder sb = new StringBuilder();
-        sb.append("CREATE");
-        if (Randomly.getBoolean()) {
-            sb.append(" UNIQUE");
-        }
-        sb.append(" INDEX ");
+
         /*
          * Commented out as a workaround for https://www.postgresql.org/message-id/CA%2Bu7OA4XYhc-
          * qyCgJqwwgMGZDWAyeH821oa5oMzm_HEifZ4BeA%40mail.gmail.com
@@ -37,64 +31,12 @@ public final class PostgresIndexGenerator extends SQLIndexGenerator {
         // sb.append("CONCURRENTLY ");
         // }
         PostgresTable randomTable = globalState.getSchema().getRandomTable(t -> !t.isView()); // TODO: materialized
-                                                                                              // views
-        String indexName = getNewIndexName(randomTable);
-        sb.append(indexName);
-        sb.append(" ON ");
-        if (Randomly.getBoolean()) {
-            sb.append("ONLY ");
-        }
-        sb.append(randomTable.getName());
-        IndexType method;
-        if (Randomly.getBoolean()) {
-            sb.append(" USING ");
-            method = Randomly.fromOptions(IndexType.values());
-            sb.append(method);
-        } else {
-            method = IndexType.BTREE;
-        }
+        // views
+        IndexType method = SQLIndexGenerator.generateHelper(
+                globalState, errors, sb, randomTable, (PostgresGlobalState state,
+                        List<PostgresColumn> columns) -> PostgresExpressionGenerator.generateExpression(state, columns),
+                PostgresVisitor::asString);
 
-        sb.append("(");
-        if (method == IndexType.HASH) {
-            sb.append(randomTable.getRandomColumn().getName());
-        } else {
-            for (int i = 0; i < Randomly.smallNumber() + 1; i++) {
-                if (i != 0) {
-                    sb.append(", ");
-                }
-                if (Randomly.getBoolean()) {
-                    sb.append(randomTable.getRandomColumn().getName());
-                } else {
-                    sb.append("(");
-                    PostgresExpression expression = PostgresExpressionGenerator.generateExpression(globalState,
-                            randomTable.getColumns());
-                    sb.append(PostgresVisitor.asString(expression));
-                    sb.append(")");
-                }
-
-                // if (Randomly.getBoolean()) {
-                // sb.append(" ");
-                // sb.append("COLLATE ");
-                // sb.append(Randomly.fromOptions("C", "POSIX"));
-                // }
-                if (Randomly.getBooleanWithRatherLowProbability()) {
-                    sb.append(" ");
-                    sb.append(globalState.getRandomOpclass());
-                    errors.add("does not accept");
-                    errors.add("does not exist for access method");
-                }
-                if (Randomly.getBoolean()) {
-                    sb.append(" ");
-                    sb.append(Randomly.fromOptions("ASC", "DESC"));
-                }
-                if (Randomly.getBooleanWithRatherLowProbability()) {
-                    sb.append(" NULLS ");
-                    sb.append(Randomly.fromOptions("FIRST", "LAST"));
-                }
-            }
-        }
-
-        sb.append(")");
         if (Randomly.getBoolean() && method != IndexType.HASH) {
             sb.append(" INCLUDE(");
             List<PostgresColumn> columns = randomTable.getRandomNonEmptyColumnSubset();
@@ -112,16 +54,4 @@ public final class PostgresIndexGenerator extends SQLIndexGenerator {
         PostgresCommon.addCommonExpressionErrors(errors);
         return new SQLQueryAdapter(sb.toString(), errors);
     }
-
-    private static String getNewIndexName(PostgresTable randomTable) {
-        List<PostgresIndex> indexes = randomTable.getIndexes();
-        int indexI = 0;
-        while (true) {
-            String indexName = DBMSCommon.createIndexName(indexI++);
-            if (indexes.stream().noneMatch(i -> i.getIndexName().equals(indexName))) {
-                return indexName;
-            }
-        }
-    }
-
 }
